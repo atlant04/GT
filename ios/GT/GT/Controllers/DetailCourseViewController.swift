f4bbad7929d26738a54f6fdaff7a0e4ead85f8d1
atlant04 <maxatlant17@gmail.com>
2020-03-19 14:22:24 -0400

    fixed calendar

--- ios/GT/GT/Controllers/DetailCourseViewController.swift
+++ ios/GT/GT/Controllers/DetailCourseViewController.swift
@@ -10,18 +10,46 @@ import Foundation
 import UIKit
 import JZCalendarWeekView
 
-class DetailCourseViewController: UIViewController {
+class DetailCourseViewController: UIViewController, MTWeekViewDataSource {
+
     var calendar: CalendarView!
     var course: Course!
+    var events: [Event]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //calendar = CalendarView(frame: view.bounds)
-        //view.addSubview(calendar)
-        setupCalendar()
+        TimeZone.ReferenceType.default = TimeZone(abbreviation: "UTC")!
+        parseEvents()
+        calendar = CalendarView(frame: view.bounds)
+        calendar.dataSource = self
+        view.addSubview(calendar)
     }
 
-    func setupCalendar() {
+    func parseEvents() {
+        guard let sections = course.sections else { return }
+        let meetings = sections.flatMap { $0.meetings }
+        print(meetings)
+        let events = meetings.flatMap { parseMeeting(meeting: $0) }
+        self.events = events
+    }
+
+    func parseMeeting(meeting: Course.Meeting) -> [Event] {
+        guard let hyphen = meeting.time.firstIndex(of: "-") else { return [] }
+        let start = meeting.time[..<hyphen]
+        let endRange = meeting.time.index(hyphen, offsetBy: 2)...
+        let end = meeting.time[endRange]
+
+        let days = Array(meeting.days).compactMap { Event.DayOfWeek(rawValue: $0) }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        if let startDate = formatter.date(from: String(start)),
+            let endDate = formatter.date(from: String(end)) {
+            return days.map { Event(meeting: meeting, startDate: startDate, endDate: endDate, dayOfWeek: $0)}
+        } else {
+            return []
+        }
 
     }
 
@@ -29,10 +57,28 @@ class DetailCourseViewController: UIViewController {
         JZWeekViewHelper.viewTransitionHandler(to: size, weekView: calendar)
     }
 
+    func calendar(eventsForDate date: Date) -> [JZBaseEvent] {
+        guard let events = self.events else { return [] }
+        let day = Calendar.current.dateComponents([.weekday], from: date)
+        let eventsForDay =  events.filter({ (event) -> Bool in
+            return event.dayOfWeek.dayIndex == day.weekday
+        })
+        return eventsForDay
+    }
+
 
 }
 
-class CalendarView: JZBaseWeekView {
+class CalendarView: MTWeekView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func registerViewClasses() {
         super.registerViewClasses()
         //self.collectionView.register(UINib(nibName: "EventCell", bundle: nil), forCellWithReuseIdentifier: EventCell.reuseIdentifier)
@@ -45,16 +91,38 @@ class CalendarView: JZBaseWeekView {
         cell.update(event: event)
         return cell
     }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let event = getCurrentEvent(with: indexPath)
+        print(event?.startDate)
+    }
 }
 
 
 class Event: JZBaseEvent {
-    override init(id: String, startDate: Date, endDate: Date) {
-        super.init(id: "Text", startDate: startDate, endDate: endDate)
+    let dayOfWeek: DayOfWeek
+    let meeting: Course.Meeting
+
+    init(meeting: Course.Meeting, startDate: Date, endDate: Date, dayOfWeek: DayOfWeek ) {
+        self.dayOfWeek = dayOfWeek
+        self.meeting = meeting
+        super.init(id: "None", startDate: startDate, endDate: endDate)
     }
 
     override func copy(with zone: NSZone?) -> Any {
-        return Event(id: id, startDate: startDate, endDate: endDate)
+        return Event(meeting: meeting, startDate: startDate, endDate: endDate, dayOfWeek: dayOfWeek)
+    }
+
+    enum DayOfWeek: Character, CaseIterable {
+        case Monday = "M"
+        case Tuesday = "T"
+        case Wednesday = "W"
+        case Thurday = "R"
+        case Friday = "F"
+
+        var dayIndex: Int {
+            return (DayOfWeek.allCases.firstIndex(of: self) ?? 0) + 2
+        }
     }
 }
 
@@ -63,13 +131,14 @@ class EventCell: UICollectionViewCell {
     static let reuseIdentifier: String = "eventCell"
 
     func update(event: Event) {
-        label.text = event.id
+        label.text = Calendar.current.dateComponents([.hour, .minute], from: event.startDate).description
     }
 
     let label: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Test"
+        label.textColor = .systemBackground
+        label.font = UIFontMetrics.default.scaledFont(for: UIFont.preferredFont(forTextStyle: .caption1), maximumPointSize: 12)
         return label
     }()
 
@@ -94,7 +163,8 @@ class EventCell: UICollectionViewCell {
             border.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             label.topAnchor.constraint(equalTo: contentView.topAnchor),
             label.leadingAnchor.constraint(equalTo: border.trailingAnchor, constant: 5),
-            label.heightAnchor.constraint(equalToConstant: 15)
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            label.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 
@@ -108,8 +178,42 @@ class EventCell: UICollectionViewCell {
         layer.shadowOffset = CGSize(width: 0, height: 4)
         layer.shadowRadius = 5
         layer.shadowOpacity = 0
-        label.font = UIFont.systemFont(ofSize: 12)
-        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
-        self.backgroundColor = UIColor(red: 238/255, green: 247/255, blue: 1, alpha: 1) //238, 247, 255
+        self.backgroundColor = .systemTeal
     }
+}
+
+
+
+protocol MTWeekViewDataSource {
+    func calendar(eventsForDate date: Date) -> [JZBaseEvent]
+}
+
+class MTWeekView: JZBaseWeekView {
+
+    var week: [Date] = Date.getCurrentWeek()
+
+    var dataSource: MTWeekViewDataSource? {
+        didSet {
+            setupCalendar()
+            self.collectionView.reloadData()
+        }
+    }
+
+    func setupCalendar() {
+        let allEvents = getAllEventsForCurrentWeek()
+        let setDate = week.first ?? Date()
+        let range = (week.first, week.last)
+        self.setupCalendar(numOfDays: 5, setDate: setDate, allEvents: allEvents, scrollType: .pageScroll, firstDayOfWeek: .Monday, currentTimelineType: .page, visibleTime: Date(), scrollableRange: range)
+    }
+
+    func getAllEventsForCurrentWeek() -> [Date: [JZBaseEvent]] {
+        var resultingEvents = [Date: [JZBaseEvent]]()
+        for day in week {
+            let dayEvents = dataSource?.calendar(eventsForDate: day)
+            resultingEvents[day] = dayEvents
+        }
+        return resultingEvents
+    }
+
+
 }
--- ios/GT/GT/Supporitng/Extensions.swift
+++ ios/GT/GT/Supporitng/Extensions.swift
@@ -53,3 +53,100 @@ extension CaseIterable {
         return array
     }
 }
+
+extension Date {
+
+  static func today() -> Date {
+      return Date()
+  }
+
+  func next(_ weekday: Weekday, considerToday: Bool = false) -> Date {
+    return get(.next,
+               weekday,
+               considerToday: considerToday)
+  }
+
+  func previous(_ weekday: Weekday, considerToday: Bool = false) -> Date {
+    return get(.previous,
+               weekday,
+               considerToday: considerToday)
+  }
+
+  func get(_ direction: SearchDirection,
+           _ weekDay: Weekday,
+           considerToday consider: Bool = false) -> Date {
+
+    let dayName = weekDay.rawValue
+
+    let weekdaysName = getWeekDaysInEnglish().map { $0.lowercased() }
+
+    assert(weekdaysName.contains(dayName), "weekday symbol should be in form \(weekdaysName)")
+
+    let searchWeekdayIndex = weekdaysName.firstIndex(of: dayName)! + 1
+
+    let calendar = Calendar(identifier: .gregorian)
+
+    if consider && calendar.component(.weekday, from: self) == searchWeekdayIndex {
+      return self
+    }
+
+    var nextDateComponent = calendar.dateComponents([.hour, .minute, .second], from: self)
+    nextDateComponent.weekday = searchWeekdayIndex
+
+    let date = calendar.nextDate(after: self,
+                                 matching: nextDateComponent,
+                                 matchingPolicy: .nextTime,
+                                 direction: direction.calendarSearchDirection)
+
+    return date!
+  }
+
+}
+
+// MARK: Helper methods
+extension Date {
+  func getWeekDaysInEnglish() -> [String] {
+    var calendar = Calendar(identifier: .gregorian)
+    calendar.locale = Locale(identifier: "en_US_POSIX")
+    return calendar.weekdaySymbols
+  }
+
+  enum Weekday: String {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+  }
+
+  enum SearchDirection {
+    case next
+    case previous
+
+    var calendarSearchDirection: Calendar.SearchDirection {
+      switch self {
+      case .next:
+        return .forward
+      case .previous:
+        return .backward
+      }
+    }
+  }
+}
+
+
+extension Date {
+    static func getCurrentWeek() -> [Date] {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+        let dayOfWeek = calendar.component(.weekday, from: today)
+        let weekdays = calendar.range(of: .weekday, in: .weekOfYear, for: today)!
+        return (weekdays.lowerBound ..< weekdays.upperBound)
+            .compactMap { calendar.date(byAdding: .day, value: $0 - dayOfWeek, to: today) }
+            .filter { !calendar.isDateInWeekend($0) }
+    }
+}
+
+extension CaseIterable where Self: Equatable {
+
+    public func ordinal() -> Self.AllCases.Index {
+        return Self.allCases.firstIndex(of: self)!
+    }
+
+}
